08ef10f49df80a441db87ee57eaff25f6e4433c1
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-07-13 17:40:45 -0300

    configurando interface

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -4,23 +4,25 @@ import { LatLng } from "leaflet";
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-export default function Mapa() {
+export default function Mapa({cidade, clinicas}) {
   // const position :  = [51.505, 51.505];
   return (
     <MapContainer
-      center={[51.505, -0.09]}
+      center={[cidade.lat, cidade.long]}
       zoom={13}
-      style={{ height: "400px", width: '100vw'  }}
+      style={{ height: "400px", width: '100vw' }}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {clinicas.map((clinica) => {
+        <Marker position={[clinica.lat, clinica.long]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      })}
     </MapContainer>
   );
 }
