50482e85b84c1e90379afb3b10088f53ae5e986b
Gustavo Priebe <40157345+gustavospriebe@users.noreply.github.com>
2023-07-13 17:39:48 -0300

    cidades.json added

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { LatLng } from "leaflet";
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
@@ -10,13 +9,13 @@ export default function Mapa() {
     <MapContainer
       center={[51.505, -0.09]}
       zoom={13}
-      style={{ height: "400px", width: '100vw'  }}
+      style={{ height: "400px", width: '100vw'}}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[51.505, -0.09]} >
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
