7cd13e68e9c799a861d3fe7cbd09c79bff59de50
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-07-13 16:11:30 -0300

    criando db fake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello world!
+      <div className="border-2 border-red w-full h-full bg-rose-600">
+        <p>Mapa</p>
+      </div>
     </main>
-  )
+  );
 }
