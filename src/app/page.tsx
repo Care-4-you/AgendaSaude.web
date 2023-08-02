0a03f2a181958a6de8d193001cb36e431923c80b
Samuel Carvalho <samuel.carvalho.dev@gmail.com>
2023-08-02 15:52:39 -0300

    alterações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Mapa from "../components/Map";
+import Mapa from "../components/Mapa";
 
 export default function Home() {
   return (
     <div className="">
-      <Mapa />
+      <Mapa cidade={{latitude: -11.337516, longitude: -38.96385}} clinicas={[{latitude: -11.337516, longitude: -38.963585, id: 1}]}/>
     </div>
   );
 }
--- src/components/Mapa/index.tsx
+++ src/components/Mapa/index.tsx
@@ -7,20 +7,22 @@ export default function Mapa({cidade, clinicas}) {
   // const position :  = [51.505, 51.505];
   return (
     <MapContainer
-      center={[cidade.lat, cidade.long]}
-      zoom={13}
-      style={{ height: "400px", width: '100vw'}}
+      center={[cidade.latitude, cidade.longitude]}
+      zoom={14}
+      style={{ height: "700px", width: '100vw'}}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {clinicas.map((clinica) => {
-        <Marker position={[clinica.lat, clinica.long]}>
+      {clinicas.length > 0 && clinicas.map((clinica) => {
+        return (
+        <Marker position={[clinica.latitude, clinica.longitude]} key={clinica.id}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
         </Marker>
+        )
       })}
     </MapContainer>
   );
