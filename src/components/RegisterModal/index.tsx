eb57ae773ef8d09b9294bad3f444c169f31ffe2b
GabrielMello1407 <gabrielmellomoraes1407@gmail.com>
2023-12-12 22:53:34 -0300

    feat: create pop-up register

---
+++ src/components/RegisterModal/index.tsx
@@ -0,0 +1,14 @@
+
+const RegisterModal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+      <div className="bg-white p-6 rounded-lg shadow-md">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default RegisterModal;
