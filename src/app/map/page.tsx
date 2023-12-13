c4b8a25d274fc3a7a68bde06df74d176f2e4cdec
GabrielMello1407 <gabrielmellomoraes1407@gmail.com>
2023-12-12 22:51:55 -0300

    feat: new map folder

---
+++ src/app/map/page.tsx
@@ -0,0 +1,39 @@
+import Mapa from "../../components/Mapa";
+import { api } from "@/lib/api";
+import { IClinica } from "@/shared/interfaces/IClinica";
+
+export default async function Home() {
+  const response = await api.get("/clinicas");
+
+  const clinicas: IClinica[] = response.data;
+
+  return (
+    <div>
+      <header className="flex flex-col gap-2 bg-roxo p-2">
+        <h1 className="align-middle text-3xl font-bold text-white">
+          Agenda Saúde
+        </h1>
+        <div className="grid w-full gap-2 md:flex lg:gap-4">
+          <input
+            type="text"
+            placeholder="Cidade"
+            className="w-full rounded-md p-1 px-4 sm:max-w-sm"
+          />
+          <input
+            type="text"
+            placeholder="Especialidade"
+            className="w-full rounded-md p-1 px-4 sm:max-w-sm"
+          />
+          <button className="w-full rounded-md bg-white p-1 px-4 lg:ml-4">
+            Encontrar clínicas
+          </button>
+        </div>
+      </header>
+      <main>
+        <div className="flex">
+          <Mapa clinicas={clinicas} />
+        </div>
+      </main>
+    </div>
+  );
+}
