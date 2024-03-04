3e85f42e8727b223ea629c9d37b2b94fd0496a29
GabrielMello1407 <gabrielmellomoraes1407@gmail.com>
2024-03-04 20:16:56 -0300

    added division of the clinic registration page

--- src/app/registerClinical/page.tsx
+++ src/app/registerClinical/page.tsx
@@ -1,21 +1,33 @@
 "use client";
-import { useEffect, useState } from "react";
-
-import { Progress } from "@/components/ui/progress";
 
 export default function registerClinical() {
-  const [progress, setProgress] = useState<number>(13);
-
-  useEffect(() => {
-    const timer = setTimeout(() => setProgress(66), 500);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <div>
-      <Progress value={progress} className="w-[60%]" />
-
-      <h1>Inserir os dados do cadastro da clinica</h1>
+    <div className="min-h-screen   bg-gradient-to-r from-white from-50% to-gray-500 to-50%">
+      <div className="flex justify-center container">
+        <div className="flex-1 gap-6 flex flex-col">
+          <hgroup className="max-w-sm pr-4">
+            <h2 className="pt-7 text-5xl font-bold text-black">
+              Lorem ipsum dolor sit amet.
+            </h2>
+          </hgroup>
+          <p className=" text-base text-gray-900 max-w-md pr-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non
+            sollicitudin tellus. Vivamus venenatis libero suscipit sem venenatis
+            finibus. Maecenas orci enim, sodales quis porttitor a, feugiat in
+            est.
+          </p>
+        </div>
+        <div className="flex-1">
+          <div className="">
+            <div className="">
+              {/* Lado direito form multistep */}
+              <form action="">
+                <h1>Inserir os dados do cadastro da clinica</h1>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
