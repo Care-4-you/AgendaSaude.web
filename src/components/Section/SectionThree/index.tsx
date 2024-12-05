9b61ee4f92b086c6882dd765ee9bc20edb9e2204
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-12-05 16:37:43 -0300

    feat: added news texts

--- src/components/Section/SectionThree/index.tsx
+++ src/components/Section/SectionThree/index.tsx
@@ -1,26 +1,29 @@
+import { Container } from "@/components/Container";
 import Link from "next/link";
 
 const SectionThree = () => {
   return (
-    <div className="bg-white p-10">
-      <div className="flex w-full max-w-4xl bg-[#1C226B] rounded-3xl">
-        <div className="w-auto h-[650px] ">
-          <h3 className="text-white font-Poppins">Perguntas e Respostas</h3>
-          <h1 className="text-white font-MuseoModerno">
-            Tire suas dúvidas e aproveite ao máximo as funcionalidades do Agenda
-            Saúde.
-          </h1>
-          <p className="text-white font-Poppins">
-            Não encontrou o que procurava?{" "}
-            <Link href={"/"}>Fale com o nosso time</Link>
-          </p>
-        </div>
-        {/* Pop-Up */}
-        <div>
-          <h1>Pop up</h1>
+    <Container>
+      <div className="bg-white p-10">
+        <div className="flex w-full max-w-4xl bg-[#1C226B] rounded-3xl">
+          <div className="w-auto h-[650px] ">
+            <h3 className="text-white font-Poppins">Perguntas e Respostas</h3>
+            <h1 className="text-white font-MuseoModerno">
+              Tire suas dúvidas e aproveite ao máximo as funcionalidades do
+              Agenda Saúde.
+            </h1>
+            <p className="text-white font-Poppins">
+              Não encontrou o que procurava?{" "}
+              <Link href={"/"}>Fale com o nosso time</Link>
+            </p>
+          </div>
+          {/* Pop-Up */}
+          <div>
+            <h1>Pop up</h1>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 };
 
