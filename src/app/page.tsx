6f059d03d3842e0171f4c447fe68ea5daa60c1eb
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-12-05 16:21:26 -0300

    feat: added carrousel and article component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,14 @@
 import Carrousel from "@/components/Carrousel";
 
-import Section from "@/components/ui/section";
+import SectionOne from "@/components/Section/SectionOne";
+import SectionTwo from "@/components/Section/SectionTwo";
 
-import { Container } from "../components/Container";
-
-import { FaGithub, FaLinkedin } from "react-icons/fa";
 export default function Home() {
   return (
-    <Container>
+    <div>
       <Carrousel />
-      <Section
-        imageSrc="/logo_soujunior.png"
-        title="Produto sem fins lucrativos"
-        description={
-          <p>
-            Esse MVP de produto digital é fruto de uma iniciativa do SouJunior
-            Labs. Maiores informações:
-          </p>
-        }
-        links={[
-          {
-            label: <FaLinkedin size={40} />,
-            href: " https://www.linkedin.com/company/soujunior-labs/"
-          },
-          {
-            label: <FaGithub size={40} />,
-            href: "https://github.com/SouJunior-Labs/project-list/wiki"
-          }
-        ]}
-      />
-    </Container>
+      <SectionOne />
+      <SectionTwo />
+    </div>
   );
 }
