cbd1ec41a574cd0a72aaf9677ca512a7dce55a6a
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-12-05 14:29:02 -0300

    refactor: refactor this component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,18 +3,14 @@ import Link from "next/link";
 
 interface LogoProps {
   imagePath: string;
+  width: number;
+  height: number;
 }
 
-const Logo: React.FC<LogoProps> = ({ imagePath }) => {
+const Logo: React.FC<LogoProps> = ({ imagePath, height, width }) => {
   return (
     <Link href="/">
-      <Image
-        src={imagePath}
-        alt="Logo"
-        width={48}
-        height={48}
-        className=" bg-slate-200 rounded-full"
-      />
+      <Image src={imagePath} alt="Logo" width={width} height={height} />
     </Link>
   );
 };
