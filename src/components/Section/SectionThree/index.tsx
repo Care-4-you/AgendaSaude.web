86430da11fdfeda50e785c518c58a3ff14136645
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-12-06 22:04:04 -0300

    feat: created section 3

--- src/components/Section/SectionThree/index.tsx
+++ src/components/Section/SectionThree/index.tsx
@@ -1,28 +1,30 @@
 import { Container } from "@/components/Container";
+import FAQItems from "@/components/ui/facItems";
 import Link from "next/link";
 
 const SectionThree = () => {
   return (
     <Container>
-      <div className="bg-white p-10">
-        <div className="flex w-full max-w-4xl bg-[#1C226B] rounded-3xl">
-          <div className="w-auto h-[650px] ">
-            <h3 className="text-white font-Poppins">Perguntas e Respostas</h3>
-            <h1 className="text-white font-MuseoModerno">
-              Tire suas dúvidas e aproveite ao máximo as funcionalidades do
-              Agenda Saúde.
-            </h1>
-            <p className="text-white font-Poppins">
-              Não encontrou o que procurava?{" "}
-              <Link href={"/"}>Fale com o nosso time</Link>
-            </p>
-          </div>
-          {/* Pop-Up */}
-          <div>
-            <h1>Pop up</h1>
-          </div>
+      <section className=" mt-6 mb-6 bg-[#1C226B] text-white p-8 rounded-3xl flex flex-col md:flex-row items-start justify-between md:items-center">
+        <div className="mb-8 md:mb-0 md:mr-8 max-w-md">
+          <h2 className="text-lg font-semibold font-Poppins">
+            Perguntas e Respostas
+          </h2>
+          <p className="text-3xl font-bold mt-2 leading-tight font-MuseoModerno">
+            Tire suas dúvidas e aproveite ao máximo as funcionalidades do Agenda
+            Saúde.
+          </p>
+          <p className="text-sm mt-4 font-Poppins text-[12px]">
+            Não encontrou o que procurava?{" "}
+            <Link href={"/"} className="underline text-blue-300">
+              Fale com o nosso time
+            </Link>
+          </p>
         </div>
-      </div>
+        <div className="w-full md:w-1/2">
+          <FAQItems />
+        </div>
+      </section>
     </Container>
   );
 };
