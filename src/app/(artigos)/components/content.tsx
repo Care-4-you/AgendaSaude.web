8f2233fbe1fdeb8400bfa16921b8ae96e8c38996
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2024-12-08 19:23:11 -0300

    feat: content component

---
+++ src/app/(artigos)/components/content.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+interface ContentOverlayProps {
+  title?: string;
+  content?: string;
+  listItems?: string[];
+  contentTwo?: string;
+}
+
+export const ContentOverlay: React.FC<ContentOverlayProps> = ({
+  title,
+  content,
+  listItems,
+  contentTwo
+}) => {
+  return (
+    <div className="absolute inset-0 z-0 flex flex-col justify-end p-4 sm:p-6 ">
+      <div className="bg-[#1C226B] p-4 rounded-3xl w-full max-w-[95%] mx-auto overflow-hidden">
+        <h1 className="text-xl sm:text-2xl font-MuseoModerno font-medium text-white bg-[#4AA9AF] bg-opacity-70 px-4 py-2 rounded-full mb-4 max-w-max">
+          {title}
+        </h1>
+
+        <p className="text-white text-sm sm:text-base leading-5 sm:leading-6 mb-4">
+          {content}
+        </p>
+
+        {listItems && listItems.length > 0 && (
+          <ol className="list-decimal list-inside text-white text-sm sm:text-base leading-5 sm:leading-6 space-y-2 mb-4">
+            {listItems.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ol>
+        )}
+
+        <p className="text-white text-sm sm:text-base leading-5 sm:leading-6 ">
+          {contentTwo}
+        </p>
+      </div>
+    </div>
+  );
+};
