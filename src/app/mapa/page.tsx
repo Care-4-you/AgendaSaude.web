cee85145cd95782f38bc47b5fb3268075cba0786
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2025-07-09 23:09:08 -0300

    removed mocked data

--- src/app/mapa/page.tsx
+++ src/app/mapa/page.tsx
@@ -1,19 +1,12 @@
-import dynamic from "next/dynamic";
-import db from "./_utils/db.json" assert { type: "json" };
 import { Filter } from "./_components/filter";
-import { ClinicaAPI } from "./types";
 
-const clinicasData: { clínicas: ClinicaAPI[] } = db;
-
-const DynamicMap = dynamic(() => import("@/app/mapa/_components/map"), {
-  ssr: false
-});
+import Map from "./_components/map";
 
 export default function MapPage() {
   return (
     <main>
       <Filter />
-      <DynamicMap clínicas={clinicasData.clínicas} />
+      <Map clínicas={[]} />
     </main>
   );
 }
